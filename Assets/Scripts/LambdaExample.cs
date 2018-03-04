d54a33dda3ba0ef4c0e66ca89fc73356b1177994
David Antognoli <david.antognoli@gmail.com>
2018-03-04 13:35:18 -0600

    initial project add

---
+++ Assets/Scripts/LambdaExample.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaExample : MonoBehaviour {
+    public event Action<float> TimerExpiredWithParameter;
+    public event Action TimerExpired;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        StartCoroutine(Timer());
+        StartCoroutine(CallFunctionAfterTimer(2, () => 
+        {
+            Debug.Log("Other timer expired");
+            Debug.Log("This is a lambda with multiple statements!");
+            Debug.Log("Microsoft says you shouldn't really do more than two or three statements, though.");
+            // Source: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/lambda-expressions#statement-lambdas
+        }));
+	}
+    
+    private IEnumerator Timer()
+    {
+        float timerDurationInSeeconds = 2;
+
+        yield return new WaitForSeconds(timerDurationInSeeconds);
+
+        if (TimerExpired != null)
+            TimerExpired.Invoke();
+
+        if (TimerExpiredWithParameter != null)
+            TimerExpiredWithParameter.Invoke(timerDurationInSeeconds);
+    }
+
+    private void OnTimerExpired()
+    {
+        Debug.Log("Timer expired!");
+    }
+
+    private void OnTimerExpiredWithParameter(float x)
+    {
+        Debug.Log(String.Format("Timer was {0} seconds.", x));
+    }
+
+    private void OnEnable()
+    {
+        TimerExpired += OnTimerExpired;
+        TimerExpired += () => Debug.Log("called from anonymous function!");
+
+        TimerExpiredWithParameter += OnTimerExpiredWithParameter;
+        TimerExpiredWithParameter += x => Debug.Log(String.Format("Anonymous function with parameter: {0}!", x + 2));
+    }
+
+    private void OnDisable()
+    {
+        TimerExpired -= OnTimerExpired;
+        TimerExpiredWithParameter -= OnTimerExpiredWithParameter;
+    }
+
+    private IEnumerator CallFunctionAfterTimer(float timerDurationInSeconds, Action functionToCall)
+    {
+        yield return new WaitForSeconds(timerDurationInSeconds);
+
+        if (functionToCall != null)
+            functionToCall.Invoke();
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerCharacter : MonoBehaviour
+{
+    #region serialized fields
+    [SerializeField]
+    private float moveSpeed = 10;
+    #endregion
+
+    #region private fields
+    private Rigidbody2D rigidbody;
+    private Text playerIndexLabel;
+    private Player controllingPlayer_UseProperty;
+    #endregion
+
+    #region public properties
+    // Which player controls the character?
+    // We will use the Player.PlayerNumber to
+    // decide which GamePad to look at.
+    public Player ControllingPlayer
+    {
+        get { return controllingPlayer_UseProperty; }
+        set
+        {
+            controllingPlayer_UseProperty = value;
+            UpdatePlayerIndexLabelText();
+        }
+    }
+    #endregion
+
+    #region private properties
+    private float HorizontalInput
+    {
+        get { return Input.GetAxis(HorizontalInputName); }
+    }
+
+    private float VerticalInput
+    {
+        get { return Input.GetAxis(VerticalInputName); }
+    }
+
+    // You must configure the Unity Input Manager
+    // to have an axis for each control for each supported player.
+    // Begin numbering at 1, as Unity numbers "joysticks" beginning at 1.
+    // "Joystick" means gamepad in real life...
+    private string HorizontalInputName
+    {
+        get
+        {
+            return "Horizontal" + ControllingPlayer.PlayerNumber;
+        }
+    }
+
+    private string VerticalInputName
+    {
+        get
+        {
+            return "Vertical" + ControllingPlayer.PlayerNumber;
+        }
+    }
+
+    private string FireInputName
+    {
+        get
+        {
+            return "Fire" + ControllingPlayer.PlayerNumber;
+        }
+    }
+    #endregion
+
+    #region MonoBehaviour functions
+    private void Awake ()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        playerIndexLabel = GetComponentInChildren<Text>();
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate ()
+    {
+        Move();
+	}
+    #endregion
+
+    private void UpdatePlayerIndexLabelText()
+    {
+        playerIndexLabel.text = ControllingPlayer.PlayerNumber.ToString();
+    }
+
+    private void Move()
+    {
+        var moveDirection = new Vector2(HorizontalInput, VerticalInput);
+        rigidbody.velocity = moveDirection * moveSpeed;
+    }
+}
